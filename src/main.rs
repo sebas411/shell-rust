31531111f4c9086a5b6156363e50ebf8b0f1ba6a
smaldonado411 <smaldonado@distelsa.com.gt>
2025-10-15 10:17:56 -0600

    Add exit builtin

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use std::io::{self, Write};
+use std::{io::{self, Write}, process};
 
 fn main() {
     let mut input;
+    let error_code;
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
@@ -9,6 +10,27 @@ fn main() {
         // Wait for user input
         input = String::new();
         io::stdin().read_line(&mut input).unwrap();
-        println!("{}: command not found", input.trim());
+
+        let try_split = input.trim().split_once(' ');
+        let command;
+        let mut args: &str = &String::new();
+    
+        if try_split.is_some() {
+            (command, args) = try_split.unwrap();
+        } else {
+            command = input.trim();
+        }
+
+        if command == "exit" {
+            if args != "" {
+                error_code = i32::from_str_radix(args, 10).unwrap();
+            } else {
+                error_code = 0;
+            }
+            break;
+        } else {
+            println!("{}: command not found", command);
+        }
     }
+    process::exit(error_code)
 }
