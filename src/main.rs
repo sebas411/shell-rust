0da8370d0fe9264c244802bb501da38ce7cce574
smaldonado411 <smaldonado@distelsa.com.gt>
2025-10-15 11:07:27 -0600

    Add functionality to search executables to type builtin

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,29 @@
-use std::{io::{self, Write}, process};
+use std::{env, fs, io::{self, Write}, os::unix::fs::PermissionsExt, process};
+
+fn find_executable(executable_name: &str) -> Option<String> {
+    let path_var = env::var("PATH").unwrap();
+    for dir_name in path_var.split(":") {
+        let result = fs::read_dir(dir_name);
+        if result.is_err() {
+            continue;
+        }
+        let files = result.unwrap();
+        for file in files {
+            let file = file.unwrap().path();
+            if file.is_file() {
+                let metadata = fs::metadata(&file).unwrap();
+                let permissions = metadata.permissions();
+                let mode: u16 = permissions.mode() as u16;
+                let executable: u16 = 493u16;
+                let is_executable = (mode & executable) == executable;
+                if is_executable && file.file_name().unwrap() == executable_name {
+                    return Some(String::from(file.to_str().unwrap()));
+                }
+            }
+        }
+    }
+    return None;
+}
 
 fn main() {
     let mut input;
@@ -40,7 +65,18 @@ fn main() {
                     break;
                 }
             }
-            if !found_builtin {
+            if found_builtin {
+                continue;
+            }
+            let result = find_executable(args);
+            let found_executable = result.is_some();
+
+            if found_executable {
+                let executable_path = result.unwrap();
+                println!("{} is {}", args, executable_path);
+            }
+
+            if  !found_executable {
                 println!("{}: not found", args);
             }
         } else {
