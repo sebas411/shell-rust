b7f5c64ceda02baa5d3e4b13e8f5e165bcfa9422
smaldonado411 <smaldonado@distelsa.com.gt>
2025-10-15 09:55:51 -0600

    Implemented REPL

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 use std::io::{self, Write};
-use std::process;
 
 fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
-
-    // Wait for user input
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
-    println!("{}: command not found", input.trim());
-    process::exit(1);
+    let mut input;
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+    
+        // Wait for user input
+        input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        println!("{}: command not found", input.trim());
+    }
 }
